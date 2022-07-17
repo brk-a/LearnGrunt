1bbf4a829c36abcf1f2f308f608bde28b67f8eb6
brk-a <fnjakai@outlook.com>
2022-07-17 09:18:10 +0300

    adding a plugin: uglifying JS

--- 0x00-LearnGrunt/Gruntfile.js
+++ 0x00-LearnGrunt/Gruntfile.js
@@ -19,12 +19,21 @@ module.exports = function(grunt){
                     dest:'css/styles.css'
                 }]
             }
+        },
+        uglify: {
+            build: {
+                files: [{
+                    src: 'build/scripts.js',
+                    dest: 'build/scripts.js' //override the file `scripts.js`
+                }]
+            }
         }
     });
 
     //load plug-ins
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     //register tasks
     grunt.registerTask('concat-js', ['concat:js']); //concats js only
@@ -33,12 +42,11 @@ module.exports = function(grunt){
 
 /**
  * on terminal, type:
- * `npm install node-sass --save-dev` //NOT A GRUNT PLUG-IN
- * `npm install grunt-sass --save-dev`
- * `grunt sass`
- * `grunt concat`
- * `grunt concat-js`
- * `grunt concat-css`
+ * `npm install grunt-contrib-uglify --save-dev`
+ * `grunt uglify`
+ * `grunt concat`  //concats css and js
+ * `grunt concat-js` //concats js only
+ * `grunt concat-css` //concats css only
  * 
  * RTFM. Flow of one plug-in is not necessarily similar to another
  */

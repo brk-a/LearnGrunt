f932a06fdbdd8370d127c40243ba2de483893ad4
brk-a <fnjakai@outlook.com>
2022-07-17 09:10:54 +0300

    adding a plugin: compiling SASS

--- 0x00-LearnGrunt/Gruntfile.js
+++ 0x00-LearnGrunt/Gruntfile.js
@@ -3,19 +3,28 @@ module.exports = function(grunt){
     //configure
     grunt.initConfig({
         concat: {
-            js:{
+            js: {
                 src: ['js/*.js'], //regex. regex everywhere
                 dest: 'build/scripts.js'
             },
             css:{
-                src: ['css/*.css'],
+                src: ['css/reset.css', 'css/bootstrap.css', 'css/styles.css'],
                 dest: 'styles.css'
+            }
+        },
+        sass: {
+            build: {
+                files: [{
+                    src: 'css/sass/styles.scss',
+                    dest:'css/styles.css'
+                }]
             }
         }
     });
 
     //load plug-ins
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-sass');
 
     //register tasks
     grunt.registerTask('concat-js', ['concat:js']); //concats js only
@@ -24,6 +33,9 @@ module.exports = function(grunt){
 
 /**
  * on terminal, type:
+ * `npm install node-sass --save-dev` //NOT A GRUNT PLUG-IN
+ * `npm install grunt-sass --save-dev`
+ * `grunt sass`
  * `grunt concat`
  * `grunt concat-js`
  * `grunt concat-css`
